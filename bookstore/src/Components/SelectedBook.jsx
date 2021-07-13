292dcd95538f3da04c94e39c9cb02f93123c7cb9
Luna Sarmiento <selene942008@hotmail.com>
2021-07-13 15:44:46 +0200

    SingleBook converted

--- bookstore/src/Components/SelectedBook.jsx
+++
@@ -1,3 +0,0 @@
-const selectedBook = (prop) => {
-  return prop.id;
-};
--- bookstore/src/Components/SingleBook.jsx
+++ bookstore/src/Components/SingleBook.jsx
@@ -1,42 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import { Card } from "react-bootstrap";
 
-class SingleBook extends React.Component {
-  state = {
-    selected: false,
-    selectedBook: null,
-  };
+const SingleBook = (prop) => {
+  const [selected, setSelected] = useState(false);
 
-  render() {
-    return (
-      <>
-        <Card
-          onClick={() =>
-            this.setState({
-              selected: !this.state.selected,
-              selectedBook: <selectedBook id={this.props.b.asin} />,
-            })
-          }
-          style={{
-            border: this.state.selected ? "3px solid red" : "none",
-            height: "100%",
-          }}
-        >
-          <Card.Img
-            variant="top"
-            src={this.props.b.img}
-            className="img-fluid w-100"
-            style={{ height: "80%" }}
-          />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>
-              {this.props.b.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Card
+        onClick={() => setSelected(!selected)}
+        style={{
+          border: selected ? "3px solid red" : "none",
+          height: "100%",
+        }}
+      >
+        <Card.Img
+          variant="top"
+          src={prop.b.img}
+          className="img-fluid w-100"
+          style={{ height: "80%" }}
+        />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{prop.b.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
